123c80ab5375ea302bc5ab0124288c388f95a2b0
Raphael Rosa <rafarbr@gmail.com>
2023-05-22 20:06:18 -0300

    Revert "fix: expose rules by field"
    This reverts commit 39469634c0909736ca1e632757f618339c396616.

--- Sources/Livr/Validator.swift
+++ Sources/Livr/Validator.swift
@@ -234,7 +234,6 @@ public final class Validator {
     @discardableResult
     public func validate(data: [String: Any?]) throws -> Output? {
         errors = nil
-        rulesByField?.removeAll()
         try setRulesByField()
 
         validatingData = data
@@ -324,5 +323,7 @@ public final class Validator {
                 }
             }
         }
+
+        rulesByField?.removeValue(forKey: field)
     }
 }
