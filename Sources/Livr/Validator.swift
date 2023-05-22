f957a3dfdfa86b9bdb7f9b1071d4e59a25eec069
Raphael Rosa <rafarbr@gmail.com>
2023-05-22 20:06:18 -0300

    Revert "fix: expose rules by field"
    This reverts commit fecad0a861cd20f2d5ef49863c9c1cb6f98a8398.

--- Sources/Livr/Validator.swift
+++ Sources/Livr/Validator.swift
@@ -52,8 +52,8 @@ public final class Validator {
         }
     }
 
-    public typealias Field = String
-    public typealias Rules = [LivrRule]
+    typealias Field = String
+    typealias Rules = [LivrRule]
 
     typealias RuleName = String
     typealias RuleArguments = Any
@@ -82,7 +82,7 @@ public final class Validator {
     private var privateRulesByField: [Field: Rules]?
 
     /// public rulesByField property - applies thread safety to the rulesByField dictionary
-    public private(set) var rulesByField: [Field: Rules]? {
+    private(set) var rulesByField: [Field: Rules]? {
         get {
             var rulesByField: [Field: Rules]? = nil
             queue.sync {
